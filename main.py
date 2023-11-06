aa2837167c7052cac9aeaea5a080297ed07b464b
Dziad573 <krystiansmagacz05@gmail.com>
2023-11-06 17:53:28 +0100

    Dodano funkcjonalność przycisków
    Historia operacji już działa, zmieniono niektóre nazwy, po kliknięciu w przyciski dopasowane im wartości pojawiają się na ekranie.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-przyciski = ['%', 'x^2', 'C', '\u21BA', '=', '0', ',', '+', '\u221A', '(', ')', 'pi', '7', '8', '9', '/', '4', '5', '6', 'x', '1', '2', '3', '-', '=', '0', ',', '+']
+przyciski = ['0', '0', '0', '0', '\u221A', '%', 'x^2', 'C', '\u21BA', '(', ')', 'pi', '7', '8', '9', '/', '4', '5', '6', 'x', '1', '2', '3', '-', '=', '0', ',', '+']
 
 def inicjalizacjaOkna():
     root = tk.Tk()
@@ -9,6 +9,7 @@ def inicjalizacjaOkna():
     # root.resizable(False, False)
     return root
 
+# Ekran wyświetlania operacji
 def inicjalizacjaEkranu(root):
     ekran = tk.Text(root, bg="black", width=77, height=10, borderwidth=0, padx=10, pady=7, fg='white')
     ekran.grid(row=0, column=0, columnspan=3, sticky="n")
@@ -18,11 +19,39 @@ def inicjalizacjaKonteneraEkranu(root):
     kontenerEkranu = tk.Label(root, bg='#4e4e4e', width=90, height=53, pady=11)
     kontenerEkranu.grid(row=0, column=0, columnspan=3)
     return kontenerEkranu
+
 def label(kontenerEkranu):
     label = tk.Label(kontenerEkranu, bg="red", pady=24)
     label.grid(row=1, column=0, columnspan=3)
     return label
-def inicjalizacjaPrzyciskow(kontenerEkranu):
+
+# Operacje na przyciskach
+def klik(ekran, przycisk, oknoHistorii):
+    def obsluga():
+        if przycisk == '\u21BA':
+            bufor = ekran.get(1.0, 'end-2c')
+            ekran.delete(1.0, tk.END)
+            ekran.insert(1.0, bufor)
+        elif przycisk == 'C':
+            ekran.delete(1.0, tk.END)
+        elif przycisk == '=':
+            zaktualizujHistorie(ekran)()
+        else:
+            tekst = przycisk if przycisk != 'x^2' else '^2'
+            ekran.insert(tk.END, tekst)
+    return obsluga
+
+def zaktualizujHistorie(ekran):
+    def f():
+        tekst = ekran.get(1.0, 'end-1c')
+        for i in range(len(oknoHistorii)):
+            if not oknoHistorii[i]['text']:
+                oknoHistorii[i]['text'] = oknoHistorii[i - 1]['text']
+                oknoHistorii[i]['text'] = tekst
+                break
+    return f
+
+def inicjalizacjaPrzyciskow(kontenerEkranu, oknoHistorii):
     przyciska = [tk.Button(kontenerEkranu, text=przycisk, width=18, height=4, pady=18, padx=11, borderwidth=1) for przycisk in przyciski]
 
     j = 1
@@ -30,28 +59,29 @@ def inicjalizacjaPrzyciskow(kontenerEkranu):
         if i % 4 == 0:
             j += 1
         przyciska[i].grid(row=j, column=i % 4, columnspan=1, pady=2, padx=2)
+        przyciska[i].configure(command=klik(ekran, przyciska[i]['text'], oknoHistorii))
 
     return przyciska
 
-def inicjalizacjaKontenera(root):
-    kontener = tk.Frame(root, bg='green', width=60, height=50)
-    kontener.grid(row=0, columnspan=2, column=3)
-    return kontener
+def inicjalizacjaKonteneraHistorii(root):
+    kontenerHistorii = tk.Frame(root, bg='green', width=60, height=50)
+    kontenerHistorii.grid(row=0, columnspan=2, column=3)
+    return kontenerHistorii
 
 # ROZMIAR DLA 1 = 70 (WYSOKOŚĆ)
-def inicjalizacjaKonteneraEkranuOperacji(kontener):
+def inicjalizacjaOknaHistorii(kontenerHistorii):
     czcionka = ("Arial", 25)
-    kontenerEkranuOperacji = [tk.Label(kontener, text="to jest text", font=czcionka, anchor='w', fg="white", bg='#292929', width=19, height=2, padx=12) for i in range(10)]
-    for i in range(len(kontenerEkranuOperacji)):
-        kontenerEkranuOperacji[i].grid(row=i, column=4, columnspan=2)
-    return kontenerEkranuOperacji
+    oknoHistorii = [tk.Label(kontenerHistorii, text="", font=czcionka, anchor='w', fg="white", bg='#292929', width=19, height=2, padx=12) for i in range(10)]
+    for i in range(len(oknoHistorii)):
+        oknoHistorii[i].grid(row=i, column=4, columnspan=2)
+    return oknoHistorii
 
 if __name__ == '__main__':
     root = inicjalizacjaOkna()
     kontenerEkranu = inicjalizacjaKonteneraEkranu(root)
     ekran = inicjalizacjaEkranu(root)
     label = label(kontenerEkranu)
-    przyciska = inicjalizacjaPrzyciskow( kontenerEkranu)
-    kontener = inicjalizacjaKontenera(root)
-    kontenerEkranuOperacji = inicjalizacjaKonteneraEkranuOperacji(kontener)
+    przyciska = inicjalizacjaPrzyciskow(kontenerEkranu, 1)
+    kontenerHistorii = inicjalizacjaKonteneraHistorii(root)
+    oknoHistorii = inicjalizacjaOknaHistorii(kontenerHistorii)
     root.mainloop()
